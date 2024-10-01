cd766f2ce52b7201e8c46b7d795ff8e55e41d2e7
piruthuvi22 <piruthuviraj22@gmail.com>
2024-10-01 09:27:22 +0530

    Lessons page, Record study session page

---
+++ frontend/src/pages/RecordStudySession.tsx
@@ -0,0 +1,95 @@
+import React from "react";
+import {
+  Box,
+  Divider,
+  List,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  ListSubheader,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import {
+  Article,
+  Description,
+  LibraryBooks,
+  MenuBook,
+  Send,
+  StickyNote2,
+} from "@mui/icons-material";
+import CircularWithValueLabel, { CircularProgressWithLabel } from "../components/CircularProgress";
+
+export default function RecordStudySession() {
+  const theme = useTheme();
+
+  return (
+    <Box height={"100vh"} bgcolor={theme.palette.grey[100]}>
+      <Box
+        sx={{
+          position: "relative",
+          // height: 300,
+        }}
+      >
+        <img
+          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkOaTwJnmXUjkpj62We__JoTQ2liCX2fLcHQ&s"
+          alt="cover"
+          style={{
+            width: "100%",
+            height: 300,
+            objectFit: "cover",
+          }}
+        />
+        <Box
+          sx={{
+            position: "absolute",
+            bottom: 0,
+            top: 0,
+            left: 0,
+            right: 0,
+            // bgcolor: theme.palette.primary.main,
+            boxShadow: "inset 0px -100px 50px 0px rgba(0,0,0,0.85)",
+          }}
+        >
+          <Typography
+            variant="h3"
+            color="white"
+            position={"absolute"}
+            bottom={0}
+            padding={2}
+          >
+            Maths
+          </Typography>
+        </Box>
+      </Box>
+      <Box>
+      {/* <CircularWithValueLabel  /> */}
+
+        <Box padding={0}>
+          <List
+            sx={{ width: "100%", bgcolor: "background.paper" }}
+            component="nav"
+            aria-labelledby="nested-list-subheader"
+            subheader={
+              <ListSubheader component="div" id="nested-list-subheader">
+                Choose the lesson
+              </ListSubheader>
+            }
+          >
+            {Array.from({ length: 20 }).map((_, index) => (
+              <>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Description />
+                  </ListItemIcon>
+                  <ListItemText primary={`Chapter ${index + 1}`} />
+                </ListItemButton>
+                <Divider variant="middle" component="li" />
+              </>
+            ))}
+          </List>
+        </Box>
+      </Box>
+    </Box>
+  );
+}
